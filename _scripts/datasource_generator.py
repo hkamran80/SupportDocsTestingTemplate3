7c08541c48fe3e6e3e84a6ac5237e19d36e2bb22
Zheng <49819455+aheze@users.noreply.github.com>
2020-11-07 17:07:07 -0800

    Add edit option to TOC

--- _scripts/datasource_generator.py
+++ _scripts/datasource_generator.py
@@ -80,8 +80,8 @@ if __name__ == "__main__":
         readme = jinja2.Template(readme_file.read(), trim_blocks=True)
 
    toc = ""
-   for support_document in data:
-        toc += f"- [{support_document['title']}]({support_document['url']})" + f" ([edit](" + f"{support_document['url']}".replace("//", "//github.com/").replace(".github.io/", "/") + f"))\n"
+      for support_document in data:
+          toc += f"- [{support_document['title']}]({support_document['url']})" + f" ([edit](" + f"{support_document['url']}".replace("//", "//github.com/").replace(".github.io/", "/") + f"))\n"
         
     datasource_url = f"https://github.com/{FULL_GITHUB_REPOSITORY}".replace("//github.com/", "//raw.githubusercontent.com/").replace("/blob/", "/") + f"/{GITHUB_BRANCH}/{DATA_JSON_FILE_PATH}"
     rendered_readme = readme.render(datasource_url=datasource_url, table_of_contents=toc)
